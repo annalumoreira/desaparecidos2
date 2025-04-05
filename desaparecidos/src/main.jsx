540b90011dd51f8ecceb156406d9f889c2cd1472
Anna Luiza <annaluizaroncati@hotmail.com>
2025-04-04 23:57:02 -0400

    criando paginas e ajustando as rotas

--- desaparecidos/src/main.jsx
+++ desaparecidos/src/main.jsx
@@ -8,6 +8,7 @@ import {createBrowserRouter, RouterProvider, Router} from 'react-router-dom'
 
 import Home from './routes/Home.jsx'
 import NewPost from './routes/NewPost.jsx'
+import Post from './routes/Post.jsx'
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
       {
         path: '/new',
         element: <NewPost />,
+      },
+      {
+        path: '/posts/:id',
+        element: <Post />,
       }
     ]
   }
--- desaparecidos/src/routes/NewPost.jsx
+++ desaparecidos/src/routes/NewPost.jsx
@@ -1,10 +1,56 @@
 import React from 'react'
+import "./NewPost.css"
+import baseLink from '../axios/config'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+
 
 const NewPost = () => {
+  const navigate = useNavigate()
+
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+const createPost = async (e) => {
+  e.preventDefault();
+
+  const post = { title, body, userId: 1 };
+
+  await baseLink.post('/posts', {body: post})
+  console.log(post)
+ 
+navigate('/')
+};
+
   return (
-    <div>
-      new
+    <div className='new-post'>
+     <h2>Inserir novo post:</h2>
+     <form onSubmit={(e) => createPost(e)}>
+      <div className='form-control'>
+        <label htmlFor="title">Título:</label>
+        <input 
+        type="text" 
+        id='title' 
+        name='title' 
+        placeholder='Título do post' 
+        onChange={(e) => setTitle(e.target.value)}/>
+      </div>
+      <div className='form-control'>
+        <label htmlFor="title">Conteúdo:</label>
+        <textarea 
+        id='body' 
+        name='body' 
+        placeholder='Conteúdo do post' 
+        onChange={(e) => setBody(e.target.value)} />
+        <input 
+        type='submit' 
+        value='Criar Post' 
+        className='btn' />
+      </div>
+     </form>
     </div>
+    
   )
 }
 
